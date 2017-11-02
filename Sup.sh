a2d4643d521350309fb498a428667dae00547a4f
Josh <josh@eastfive.com>
2017-11-02 14:30:01 -0500

    cleaned up output on Sup

--- Sup.sh
+++ Sup.sh
@@ -3,12 +3,23 @@ echo "BASEDIR:$basedir"
 modules=`cat .gitmodules | grep "\[submodule" | sed -e s/.*\ \"//g | sed -e s/\"]//g`
 echo $modules
 for module in $modules; do
-	echo "CD $basedir/$module"
+	echo "CHECKING OUT:$basedir/$module"
 	dir=`echo "$basedir/$module" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//'`
 	cd $dir
-	echo "CHECKOUT....."
-	git checkout master
-	git pull origin master
+	if [ `git status | grep -i "Changes not staged for commit" | wc -l` = "0" ]
+	then
+		git checkout master > /dev/null 2>&1
+		if [ $? -eq 0 ]; then
+			git pull origin master > /dev/null 2>&1
+			if [ $? -gt 0 ]; then
+				echo "PULL FAILED!!!!"
+			fi
+		else
+			echo "CHECKOUT FAILED!!!!"
+		fi
+	else
+		echo "SUBMODULE:$module:IS NOT CLEAN"
+	fi
 	cd $basedir
 	echo "DONE"
 done
