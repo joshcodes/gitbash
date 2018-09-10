4e4c662446ee63a927c16dfff2384f0331556b0c
Josh Wingstrom <josh@eastfive.com>
2018-09-10 13:09:08 -0500

    less verbose output

--- Scommit.sh
+++ Scommit.sh
@@ -3,18 +3,35 @@ echo "BASEDIR:$basedir"
 modules=`cat .gitmodules | grep "\[submodule" | sed -e s/.*\ \"//g | sed -e s/\"]//g`
 echo $modules
 for module in $modules; do
-	echo "CHECKING OUT:$basedir/$module"
 	dir=`echo "$basedir/$module" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//'`
 	cd $dir
-	if [ `git status | grep -i "Changes not staged for commit" | wc -l` = "0" ]
+
+	if [ `git status | grep -i "Untracked files" | wc -l` -gt 0 ]
 	then
-		echo "No Changes"
-	else
+		echo "SUBMODULE:$module:Adding files"
+		git add .
+	fi
+
+	if [ `git status | grep -i 'Changes not staged for commit\|Changes to be committed' | wc -l` -gt 0 ]
+	then
 		echo "SUBMODULE:$module:Committing"
 		git commit -m "$1" -a
 	fi
+
 	cd $basedir
-	echo "DONE"
-	echo
 done
+
+if [ `git status | grep -i "Untracked files" | wc -l` -gt 0 ]
+then
+	echo "SUBMODULE:$module:Adding files"
+	git add .
+fi
+
+if [ `git status | grep -i 'Changes not staged for commit\|Changes to be committed' | wc -l` = "0" ]
+then
+	echo "No Changes to base project"
+else
+	echo "Committing base project"
+	git commit -m "$1" -a
+fi
 
--- Spush.sh
+++ Spush.sh
@@ -3,37 +3,29 @@ echo "BASEDIR:$basedir"
 modules=`cat .gitmodules | grep "\[submodule" | sed -e s/.*\ \"//g | sed -e s/\"]//g`
 echo $modules
 for module in $modules; do
-	echo "PUSHING:$basedir/$module"
 	dir=`echo "$basedir/$module" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//'`
 	cd $dir
 
 	if [ `git remote -v | grep -i "https://github.com" | wc -l` -gt 0 ]
 	then
-		echo "Converting to SSH repo"
+		echo "SUBMODULE:$module:Converting to SSH repo"
 		ToSSH.sh
 	fi
 
-	if [ `git status | grep -i "Changes not staged for commit" | wc -l` = "0" ]
+	git checkout master > /dev/null 2>&1
+	if [ $? -eq 0 ]
 	then
-		git checkout master > /dev/null 2>&1
-		if [ $? -eq 0 ]; then
-			if [ `git push origin master | grep -i "Everything up-to-date" | wc -l` = "0" ]
+		if [ `git status | grep -i "Your branch is ahead of" | wc -l` -gt 0 ]
+		then
+			git push origin master > /dev/null 2>&1
+			if [ $? -gt 0 ]
 			then
-				echo "Already up to date"
+				echo "SUBMODULE:$module:PUSH FAILED!!!!"
 			else
-				if [ $? -gt 0 ]; then
-					echo "PULL FAILED!!!!"
-				else
-					echo "Updated"
-				fi
+				echo "SUBMODULE:$module:Updated"
 			fi
-		else
-			echo "CHECKOUT FAILED!!!!"
 		fi
-	else
-		echo "SUBMODULE:$module:IS NOT CLEAN"
 	fi
 	cd $basedir
-	echo "DONE"
 done
 
